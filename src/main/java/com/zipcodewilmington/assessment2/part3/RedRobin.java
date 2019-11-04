2320f6937813c8eaafc831d574542bfea182138e
Julia Evans <julevans93@users.noreply.github.com>
2019-11-04 13:18:23 -0500

    updating progress completed redrobin

--- src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RedRobin.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class RedRobin {
+public class RedRobin extends Bird{
+    @Override
+    public void setMigrationMonth(String migrationMonth) {
+        this.migrationMonth = migrationMonth;
+    }
+
+    @Override
+    public String getMigrationMonth() {
+        return migrationMonth;
+    }
+
+    public String migrationMonth;
+
+    @Override
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public String color() {
+        return "red";
+    }
 }
