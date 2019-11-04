22057a730b74629acd4f5ac2120f4c999eac9376
Julia Evans <julevans93@users.noreply.github.com>
2019-11-04 12:33:56 -0500

    in progress completed list utility

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,21 +1,26 @@
 package com.zipcodewilmington.assessment2.part2;
 
 import com.sun.deploy.util.ArrayUtil;
+import sun.awt.image.ImageWatched;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        Integer [] mergeAll = new Integer[array1.length + array2.length];
-        for(int i = 0; i < array1.length; i++){
-            array1[i] = mergeAll[0];
-            for(int j = 0; j < array2.length; j++){
-                array2[j] = mergeAll[i+1];
-            }
-        }
-        return mergeAll;
+        return null;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        LinkedList<Integer> rotate = new LinkedList<>(Arrays.asList(array));
+        Integer counter = 0;
+        while(counter < index){
+            rotate.addLast(rotate.removeFirst());
+            counter++;
+        }
+        Integer [] rotations = new Integer [array.length];
+        return rotate.toArray(rotations);
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,22 +1,46 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.deploy.util.ArrayUtil;
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListUtility {
-    public Boolean add(int i) {
+    List<Integer> list;
+
+    public ListUtility(){
+        this.list = new LinkedList<>();
+    }
+    public Boolean add(Integer i) {
         return null;
     }
 
     public Integer size() {
-        return null;
+        return list.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+        List<Integer> result = new LinkedList<>();
+        for (Integer x : list){
+            if(!result.contains(x)) {
+                result.add(x);
+            }
+        }
+        return result;
     }
 
     public String join() {
-        return null;
+        StringBuilder result = new StringBuilder();
+        int counter =1;
+        for (Integer x : list) {
+            if (counter == list.size()) {
+                result.append(String.valueOf(x));}
+                else result.append(String.format("%d, ", x));
+                counter++;
+            }
+        return result.toString();
     }
 
     public Integer mostCommon() {
@@ -24,6 +48,6 @@ public class ListUtility {
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        return list.contains(valueToAdd);
     }
 }
