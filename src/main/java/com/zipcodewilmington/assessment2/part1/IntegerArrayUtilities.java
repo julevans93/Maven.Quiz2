362fadd39a3ab9907545d16298c3cda34c6e07ab
Julia Evans <julevans93@users.noreply.github.com>
2019-11-04 10:44:08 -0500

    in progress completed WuTang

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,26 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.Arrays;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] array = new Integer[start-stop];
+
+        return array;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array [array.length-1] * array[array.length-2];
     }
 }
