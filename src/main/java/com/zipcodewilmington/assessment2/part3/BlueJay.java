bea43c847b37c47d1a5ff71402bd1b4eb16ad6e1
Julia Evans <julevans93@users.noreply.github.com>
2019-11-04 13:14:37 -0500

    updating progress completed bluejay

--- src/main/java/com/zipcodewilmington/assessment2/part3/BlueJay.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/BlueJay.java
@@ -2,4 +2,13 @@ package com.zipcodewilmington.assessment2.part3;
 
 public class BlueJay extends Bird{
 
+    @Override
+    public int getSpeed() {
+        return 13;
+    }
+
+    @Override
+    public String color() {
+        return "blue";
+    }
 }
