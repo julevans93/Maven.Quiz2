b4296f800a01b9513a896046bc45ea1e73467c2b
Julia Evans <julevans93@users.noreply.github.com>
2019-11-04 13:56:51 -0500

    updating progress array utility

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -9,7 +9,10 @@ import java.util.LinkedList;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] result = new Integer[array1.length + array2.length];
+        System.arraycopy(array1,0, result,0,array1.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+        return result;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
@@ -24,10 +27,43 @@ public class ArrayUtility {
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        int counter1 = 0;
+        int counter2 = 0;
+        int sum = 0;
+        for (Integer x : array1){
+            if(x == valueToEvaluate);
+            counter1++;
+            for (Integer j : array2){
+                if (j == x);
+                counter2++;
+            }
+        }
+
+        return counter1 + counter2;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        Arrays.sort(array);
+        int maxCount = 1;
+        int result = array[0];
+        int currCount =1;
+
+        for (int i = 1; i < array.length; i++){
+            if (array[i] == array [i-1]){
+                currCount++;
+            }
+            else{
+                if (currCount>maxCount) {
+                    maxCount = currCount;
+                    result = array[i - 1];
+                }
+                currCount =1;
+            }
+        }
+        if (currCount > maxCount){
+            maxCount = currCount;
+            result = array[array.length-1];
+        }
+        return result;
     }
 }
